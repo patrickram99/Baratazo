ab6dab0a18b1981aa9215310ae816a5fb8236699
Patrick Ramirez <pramirezs@ulasalle.edu.pe>
2024-10-07 07:26:47 -0500

    TG-34 #done feat: add specific commits format

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,20 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [0],
+    'type-enum': [0],
+    'type-empty': [0],
+    'subject-empty': [0],
+    'header-full-stop': [0],
+    'header-case': [0],
+    'scope-empty': [0],
+    'body-leading-blank': [0],
+    'footer-leading-blank': [0],
+    'header-pattern': [
+      2,
+      'always',
+      /^TG-\d+ #(ready-for-test|done) (feat|fix|docs|style|refactor|perf|test|chore|revert): .+$/
+    ],
+  },
+};
