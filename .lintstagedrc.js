0abc03e2df58b6b2dffca7e5166023e4cda3f5d8
Patrick Ramirez <pramirezs@ulasalle.edu.pe>
2024-10-06 21:57:33 -0500

    feat: fix linter and commits

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  'backend/**/*.{js,ts}': ['eslint --fix --config backend/.eslintrc.js', 'prettier --write'],
+  'frontend/**/*.{js,jsx,ts,tsx}': ['eslint --fix --config frontend/.eslintrc.js', 'prettier --write'],
   '*.{json,md}': ['prettier --write'],
 };
