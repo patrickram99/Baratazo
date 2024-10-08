7ca1035d67876237833d3e0c496754faf23a1c2c
Patrick Ramirez <pramirezs@ulasalle.edu.pe>
2024-10-07 22:58:55 -0500

    TG-34 #done feat: final linter and ignore cors for presentation

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 import bookRouter from './routes/book.route'
+
 const app = express()
+
+// Enable CORS for all routes
+app.use(cors())
+
 app.use(express.json())
 app.use('/books', bookRouter)
+
 export default app
